c96a856d0481dfc3e3ce4ea4aa8b8b7f0e05368e
saisree <saisree@INsaisree.local>
2015-09-03 11:04:19 +0530

    Setting Up files

---
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -0,0 +1,14 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
