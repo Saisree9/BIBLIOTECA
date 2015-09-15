9dc491e418c9a53ab4b6c3e74648014926a825eb
saisree <saisree@INsaisree.local>
2015-09-15 15:57:39 +0530

    added spec for checkOutMovieOption, which should call  checkOut function

--- test/com/twu/biblioteca/CheckOutMovieOptionTest.java
+++ test/com/twu/biblioteca/CheckOutMovieOptionTest.java
@@ -15,24 +15,39 @@ public class CheckOutMovieOptionTest {
     private Movie movie1 = new Movie("Movie1", "director1", 1, 1);
     private Movie movie2 = new Movie("Movie2", "director2", 2, 2);
     private MainMenuOption checkOutMovieOption;
-    private Console console;
+    MovieStore movieStore;
+
 
     @Before
     public void setUp() {
         movies.add(movie1);
         movies.add(movie2);
-        MovieStore movieStore = new MovieStore(movies);
-        console = mock(Console.class);
-        checkOutMovieOption = new CheckOutMovieOption(console, movieStore);
+        movieStore = new MovieStore(movies);
 
+
+        checkOutMovieOption = mock(CheckOutMovieOption.class);
     }
 
     @Test
     public void shouldAskForMovieName() {
+        Console console = mock(Console.class);
+        checkOutMovieOption = new CheckOutMovieOption(console, movieStore);
         when(console.getUserInput()).thenReturn("Movie1");
 
         checkOutMovieOption.doOperation();
 
         verify(console).display("Enter movie name to checkout: ");
+    }
+
+    @Test
+    public void shouldDoCheckOutMovieOperationWhenCalled() {
+        Console console = mock(Console.class);
+        checkOutMovieOption = mock(CheckOutMovieOption.class);
+
+        when(console.getUserInput()).thenReturn("Movie1");
+
+        checkOutMovieOption.doOperation();
+
+        verify(checkOutMovieOption).doOperation();
     }
 }
