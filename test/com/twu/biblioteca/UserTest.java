b1f17e08fbc099b6e7bc9c6fc08bd0a1d2b41f82
saisree <saisree@INsaisree.local>
2015-09-16 08:43:16 +0530

    changes in readme and spec for user should equal to itself

--- test/com/twu/biblioteca/UserTest.java
+++ test/com/twu/biblioteca/UserTest.java
@@ -2,11 +2,13 @@ package com.twu.biblioteca;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class UserTest {
     @Test
     public void shouldEqualToItself() {
         User user = new User("Sai", "SX");
-
+        assertEquals(user, user);
     }
 
 }
