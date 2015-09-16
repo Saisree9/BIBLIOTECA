b86e4650e1c67590ccb32bdf8fdbc7d140edc52e
saisree <saisree@INsaisree.local>
2015-09-16 16:57:28 +0530

    added spec for user hashcode should equal to user having same username and password

--- test/com/twu/biblioteca/UserTest.java
+++ test/com/twu/biblioteca/UserTest.java
@@ -8,21 +8,29 @@ import static org.junit.Assert.assertNotEquals;
 public class UserTest {
     @Test
     public void shouldEqualToItself() {
-        User user = new User("Sai", "SX","user");
+        User user = new User("Sai", "SX", "user");
         assertEquals(user, user);
     }
 
     @Test
     public void shouldNotEqualsToNull() {
-        User user = new User("Sai", "SX","user");
+        User user = new User("Sai", "SX", "user");
         assertNotEquals(null, user);
     }
 
     @Test
-    public void shouldEqualToUserHavingSameUserName() {
-        User user1 = new User(" Sai", "S","user");
-        User user2 = new User(" Sai", "D","user");
+    public void shouldEqualToUserHavingSameUserNameAndPassword() {
+        User user1 = new User(" Sai", "S", "user");
+        User user2 = new User(" Sai", "S", "user1");
         assertEquals(user1, user2);
+
+    }
+
+    @Test
+    public void shouldEqualToAnotherUserIfHashCodeIsSame() {
+        User user1 = new User(" Sai", "S", "user");
+        User user2 = new User(" Sai", "S", "user");
+        assertEquals(user1.hashCode(), user2.hashCode());
 
     }
 }
