c1fec17ace539933810ec4473b913aad57f0d648
saisree <saisree@INsaisree.local>
2015-09-16 08:47:42 +0530

    added spec for user should not equals to null

--- test/com/twu/biblioteca/UserTest.java
+++ test/com/twu/biblioteca/UserTest.java
@@ -3,12 +3,19 @@ package com.twu.biblioteca;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class UserTest {
     @Test
     public void shouldEqualToItself() {
         User user = new User("Sai", "SX");
         assertEquals(user, user);
+    }
+
+    @Test
+    public void shouldNotEqualsToNull() {
+        User user = new User("Sai", "SX");
+        assertNotEquals(null, user);
     }
 
 }
