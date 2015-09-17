7f540e4de29ddb3ae880fcc8e18660109d81bd09
saisree <saisree@INsaisree.local>
2015-09-17 20:19:03 +0530

    added interface and added spec for Librarian options delegator should get input option from user

---
+++ test/com/twu/biblioteca/LibrarianMainMenuOptionsDelegatorTest.java
@@ -0,0 +1,44 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class LibrarianMainMenuOptionsDelegatorTest {
+    private List<Book> listOfBooks = new ArrayList<Book>();
+    private Console console = mock(Console.class);
+    private Book book1 = new Book("Java design patterns", "pankaj", 1887);
+    private Book book2 = new Book("Head First Java", "Bert", 1991);
+    private Library library;
+    private MainMenuOptionDelegator delegator;
+    private List<Movie> movies = new ArrayList<Movie>();
+    private Movie movie1 = new Movie("Movie1", "director1", 1, 1);
+    private Movie movie2 = new Movie("Movie2", "director2", 2, 2);
+    private MovieStore movieStore;
+
+    @Before
+    public void setUp() {
+        listOfBooks.add(book1);
+        listOfBooks.add(book2);
+        library = new Library(listOfBooks);
+        movies.add(movie1);
+        movies.add(movie2);
+        movieStore = new MovieStore(movies);
+        delegator = new LibrarianMainMenuOptionsDelegator();
+    }
+
+    @Test
+    public void shouldGetOptionInputFromUser() {
+        when(console.getUserInput()).thenReturn("1");
+
+        delegator.getMainMenuOption(console, library, movieStore);
+
+        verify(console).getUserInput();
+    }
+}
